6b14519cf9f9b0eb4a0ea16b41b77800147e4771
sophiamjiali <sophiamjiali@gmail.com>
2026-02-04 12:30:47 -0500

    Added metadata I/O functions to API

--- src/methyltrain/api/steps.py
+++ src/methyltrain/api/steps.py
@@ -631,3 +631,38 @@ def save_project(adata: ad.AnnData, layout: ProjectLayout) -> None:
 
     layout.validate()
     adata.write_h5ad(layout.project_adata, compression = "gzip")
+
+
+# =====| Metadata I/O |=========================================================
+
+def load_audit_table(layout: ProjectLayout) -> pd.DataFrame:
+    # Loads the audit table with file_id as index
+
+    layout.validate()
+    audit_table = pd.read_csv(layout.audit_table, sep = '\t', index_col = 0)
+    return audit_table
+
+
+def load_metadata(layout: ProjectLayout) -> pd.DataFrame:
+    # Loads the metadata table with file_id as index
+
+    layout.validate()
+    metadata = pd.read_csv(layout.metadata, sep = '\t', index_col = 0)
+    return metadata
+
+
+def load_manifest(layout: ProjectLayout) -> pd.DataFrame:
+    # Loads the manifest with file_id as index
+
+    layout.validate()
+    manifest = pd.read_csv(layout.manifest, sep = '\t', index_col = 0)
+    return manifest
+
+
+def load_status_log(layout: ProjectLayout) -> pd.DataFrame:
+    # Loads the status log with file_id as index
+    layout.validate()
+    status_log = pd.read_csv(layout.status_log, sep = '\t', index_col = 0)
+    return status_log
+
+# [END]

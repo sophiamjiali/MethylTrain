c653966b713d05f425ad82de826a9508757120bc
Sophia Li <sophiamjiali@gmail.com>
2026-05-28 14:41:25 -0400

    Adjusted paths

--- slurm/run_project.sh
+++ slurm/run_project.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --output=/cluster/projects/kumargroup/sophia/logs/projects/%x_%j.out
-#SBATCH --error=/cluster/projects/kumargroup/sophia/logs/projects/%x_%j.err
+#SBATCH --output=/cluster/home/t144807uhn/logs/%x_%j.out
+#SBATCH --error=/cluster/home/t144807uhn/logs//%x_%j.err
 #SBATCH --time=01:55:00
 #SBATCH --partition=build
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
+#SBATCH --mem=4G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sophiamjia.li@mail.utoronto.ca
 

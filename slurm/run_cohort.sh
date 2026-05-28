51eb3d03c9ca210c0248f063ab42e5dcc9c73ef3
Sophia Li <sophiamjiali@gmail.com>
2026-05-28 15:31:12 -0400

    Nested logs

--- slurm/run_cohort.sh
+++ slurm/run_cohort.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --output=/cluster/home/t144807uhn/logs/cohorts/%x_%j.out
-#SBATCH --error=/cluster/home/t144807uhn/logs/cohorts/%x_%j.err
+#SBATCH --output=/cluster/home/t144807uhn/logs/cohorts/%x/%x_%j.out
+#SBATCH --error=/cluster/home/t144807uhn/logs/cohorts/%x/%x_%j.err
 #SBATCH --time=01:55:00
 #SBATCH --partition=build
 #SBATCH --nodes=1
@@ -9,7 +9,12 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sophiamjia.li@mail.utoronto.ca
 
+# Make the project-specific logs directory
+mkdir -p /cluster/home/t144807uhn/logs/cohorts/$1
+
+# Activate the virtual environment
 module load python3/3.12.11
 source /cluster/home/t144807uhn/envs/methyltrain-env/bin/activate
 
+# Resolve the configurations based on the provided job name
 python -m methyltrain.cli cohort --name $1
